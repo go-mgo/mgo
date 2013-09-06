025771d22688bd2bc744b2f0f038173ec1173418
Gustavo Niemeyer <gustavo@niemeyer.net>
2013-09-06 11:27:33 -0300

    Increase the socket timeout used by the pinger.

--- server.go
+++ server.go
@@ -272,7 +272,7 @@ func (server *mongoServer) pinger(loop bool) {
 			time.Sleep(pingDelay)
 		}
 		op := op
-		socket, _, err := server.AcquireSocket(0, pingDelay)
+		socket, _, err := server.AcquireSocket(0, 3 * pingDelay)
 		if err == nil {
 			start := time.Now()
 			_, _ = socket.SimpleQuery(&op)
