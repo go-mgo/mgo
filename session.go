0c13dbf2a6fe4cac8b082044040f19978e6d1aa0
Las Zenow <zenow@riseup.net>
2014-09-02 00:22:47 -0500

    Add text indexes support

--- session.go
+++ session.go
@@ -910,14 +910,17 @@ func (db *Database) RemoveUser(user string) error {
 }
 
 type indexSpec struct {
-	Name, NS       string
-	Key            bson.D
-	Unique         bool ",omitempty"
-	DropDups       bool "dropDups,omitempty"
-	Background     bool ",omitempty"
-	Sparse         bool ",omitempty"
-	Bits, Min, Max int  ",omitempty"
-	ExpireAfter    int  "expireAfterSeconds,omitempty"
+	Name, NS         string
+	Key              bson.D
+	Unique           bool   ",omitempty"
+	DropDups         bool   "dropDups,omitempty"
+	Background       bool   ",omitempty"
+	Sparse           bool   ",omitempty"
+	Bits, Min, Max   int    ",omitempty"
+	ExpireAfter      int    "expireAfterSeconds,omitempty"
+	Weights          bson.D ",omitempty"
+	DefaultLanguage  string "default_language,omitempty"
+	LanguageOverride string "language_override,omitempty"
 }
 
 type Index struct {
@@ -932,14 +935,26 @@ type Index struct {
 	Name string // Index name, computed by EnsureIndex
 
 	Bits, Min, Max int // Properties for spatial indexes
+
+	// Properties for text indexes
+	DefaultLanguage  string
+	LanguageOverride string
 }
 
-func parseIndexKey(key []string) (name string, realKey bson.D, err error) {
+type indexKeyInfo struct {
+	name    string
+	key     bson.D
+	weights bson.D
+}
+
+func parseIndexKey(key []string) (*indexKeyInfo, error) {
+	var keyInfo indexKeyInfo
+	isText := false
 	var order interface{}
 	for _, field := range key {
 		raw := field
-		if name != "" {
-			name += "_"
+		if keyInfo.name != "" {
+			keyInfo.name += "_"
 		}
 		var kind string
 		if field != "" {
@@ -947,7 +962,7 @@ func parseIndexKey(key []string) (name string, realKey bson.D, err error) {
 				if c := strings.Index(field, ":"); c > 1 && c < len(field)-1 {
 					kind = field[1:c]
 					field = field[c+1:]
-					name += field + "_" + kind
+					keyInfo.name += field + "_" + kind
 				}
 			}
 			switch field[0] {
@@ -960,32 +975,40 @@ func parseIndexKey(key []string) (name string, realKey bson.D, err error) {
 				// The shell used to render this field as key_ instead of key_2d,
 				// and mgo followed suit. This has been fixed in recent server
 				// releases, and mgo followed as well.
-				name += field + "_2d"
+				keyInfo.name += field + "_2d"
 			case '-':
 				order = -1
 				field = field[1:]
-				name += field + "_-1"
+				keyInfo.name += field + "_-1"
 			case '+':
 				field = field[1:]
 				fallthrough
 			default:
 				if kind == "" {
 					order = 1
-					name += field + "_1"
+					keyInfo.name += field + "_1"
 				} else {
 					order = kind
 				}
 			}
 		}
 		if field == "" || kind != "" && order != kind {
-			return "", nil, fmt.Errorf(`invalid index key: want "[$<kind>:][-]<field name>", got %q`, raw)
+			return nil, fmt.Errorf(`invalid index key: want "[$<kind>:][-]<field name>", got %q`, raw)
 		}
-		realKey = append(realKey, bson.DocElem{field, order})
+		if kind == "text" {
+			if !isText {
+				keyInfo.key = append(keyInfo.key, bson.DocElem{"_fts", "text"}, bson.DocElem{"_ftsx", 1})
+				isText = true
+			}
+			keyInfo.weights = append(keyInfo.weights, bson.DocElem{field, 1})
+		} else {
+			keyInfo.key = append(keyInfo.key, bson.DocElem{field, order})
+		}
 	}
-	if name == "" {
-		return "", nil, errors.New("invalid index key: no fields provided")
+	if keyInfo.name == "" {
+		return nil, errors.New("invalid index key: no fields provided")
 	}
-	return
+	return &keyInfo, nil
 }
 
 // EnsureIndexKey ensures an index with the given key exists, creating it
@@ -1072,29 +1095,32 @@ func (c *Collection) EnsureIndexKey(key ...string) error {
 //     http://www.mongodb.org/display/DOCS/Multikeys
 //
 func (c *Collection) EnsureIndex(index Index) error {
-	name, realKey, err := parseIndexKey(index.Key)
+	keyInfo, err := parseIndexKey(index.Key)
 	if err != nil {
 		return err
 	}
 
 	session := c.Database.Session
-	cacheKey := c.FullName + "\x00" + name
+	cacheKey := c.FullName + "\x00" + keyInfo.name
 	if session.cluster().HasCachedIndex(cacheKey) {
 		return nil
 	}
 
 	spec := indexSpec{
-		Name:        name,
-		NS:          c.FullName,
-		Key:         realKey,
-		Unique:      index.Unique,
-		DropDups:    index.DropDups,
-		Background:  index.Background,
-		Sparse:      index.Sparse,
-		Bits:        index.Bits,
-		Min:         index.Min,
-		Max:         index.Max,
-		ExpireAfter: int(index.ExpireAfter / time.Second),
+		Name:             keyInfo.name,
+		NS:               c.FullName,
+		Key:              keyInfo.key,
+		Unique:           index.Unique,
+		DropDups:         index.DropDups,
+		Background:       index.Background,
+		Sparse:           index.Sparse,
+		Bits:             index.Bits,
+		Min:              index.Min,
+		Max:              index.Max,
+		ExpireAfter:      int(index.ExpireAfter / time.Second),
+		Weights:          keyInfo.weights,
+		DefaultLanguage:  index.DefaultLanguage,
+		LanguageOverride: index.LanguageOverride,
 	}
 
 	session = session.Clone()
@@ -1123,13 +1149,13 @@ func (c *Collection) EnsureIndex(index Index) error {
 //
 // See the EnsureIndex method for more details on indexes.
 func (c *Collection) DropIndex(key ...string) error {
-	name, _, err := parseIndexKey(key)
+	keyInfo, err := parseIndexKey(key)
 	if err != nil {
 		return err
 	}
 
 	session := c.Database.Session
-	cacheKey := c.FullName + "\x00" + name
+	cacheKey := c.FullName + "\x00" + keyInfo.name
 	session.cluster().CacheIndex(cacheKey, false)
 
 	session = session.Clone()
@@ -1141,7 +1167,7 @@ func (c *Collection) DropIndex(key ...string) error {
 		ErrMsg string
 		Ok     bool
 	}{}
-	err = db.Run(bson.D{{"dropIndexes", c.Name}, {"index", name}}, &result)
+	err = db.Run(bson.D{{"dropIndexes", c.Name}, {"index", keyInfo.name}}, &result)
 	if err != nil {
 		return err
 	}
@@ -2254,8 +2280,8 @@ func (q *Query) Explain(result interface{}) error {
 //
 func (q *Query) Hint(indexKey ...string) *Query {
 	q.m.Lock()
-	_, realKey, err := parseIndexKey(indexKey)
-	q.op.options.Hint = realKey
+	keyInfo, err := parseIndexKey(indexKey)
+	q.op.options.Hint = keyInfo.key
 	q.op.hasOptions = true
 	q.m.Unlock()
 	if err != nil {
