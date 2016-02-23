b6e2fa371e64216a45e61072a96d4e3859f169da
Livio Soares <lsoares@us.ibm.com>
2016-02-23 08:38:14 -0500

    Merge embedded structs fix for Go 1.6 from unstable.

--- bson/bson_test.go
+++ bson/bson_test.go
@@ -1053,6 +1053,13 @@ type inlineDupMap struct {
 type inlineBadKeyMap struct {
 	M map[int]int ",inline"
 }
+type inlineUnexported struct {
+	M          map[string]interface{} ",inline"
+	unexported         ",inline"
+}
+type unexported struct {
+	A int
+}
 
 type getterSetterD bson.D
 
@@ -1284,6 +1291,7 @@ var twoWayCrossItems = []crossTypeItem{
 	{&inlineMapInt{A: 1, M: map[string]int{"b": 2}}, map[string]int{"a": 1, "b": 2}},
 	{&inlineMapInt{A: 1, M: nil}, map[string]int{"a": 1}},
 	{&inlineMapMyM{A: 1, M: MyM{"b": MyM{"c": 3}}}, map[string]interface{}{"a": 1, "b": map[string]interface{}{"c": 3}}},
+	{&inlineUnexported{M: map[string]interface{}{"b": 1}, unexported: unexported{A: 2}}, map[string]interface{}{"b": 1, "a": 2}},
 
 	// []byte <=> Binary
 	{&struct{ B []byte }{[]byte("abc")}, map[string]bson.Binary{"b": bson.Binary{Data: []byte("abc")}}},
