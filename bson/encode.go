f3462d37ab5d4cb9fc98bbb25265d10ddbeb9b41
Livio Soares <lsoares@us.ibm.com>
2016-02-23 08:56:19 -0500

    bson: Fix for ,inline-d structs of unexported embedded for Go 1.6.
    Simply apply the recommendations described in the release document:
    https://golang.org/doc/go1.6#reflect
    
    To modify this check `f.PkgPath != ""` to this check `f.PkgPath != "" && !f.Anonymous`.

--- bson/encode.go
+++ bson/encode.go
@@ -188,7 +188,7 @@ func isZero(v reflect.Value) bool {
 			return v.Interface().(time.Time).IsZero()
 		}
 		for i := 0; i < v.NumField(); i++ {
-			if vt.Field(i).PkgPath != "" {
+			if vt.Field(i).PkgPath != "" && !vt.Field(i).Anonymous {
 				continue // Private field
 			}
 			if !isZero(v.Field(i)) {
