20e84f37df5c4f1c8c6c51373b7b5b8b0fa83e6b
John Arbash Meinel <john@arbash-meinel.com>
2017-06-06 13:55:05 +0400

    run 'go fmt' using go 1.8

--- txn/sim_test.go
+++ txn/sim_test.go
@@ -2,11 +2,11 @@ package txn_test
 
 import (
 	"flag"
+	. "gopkg.in/check.v1"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"gopkg.in/mgo.v2/dbtest"
 	"gopkg.in/mgo.v2/txn"
-	. "gopkg.in/check.v1"
 	"math/rand"
 	"time"
 )
