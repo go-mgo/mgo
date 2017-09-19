a90f58c4c4fc61c77e7cdd256f9b1dd2b4de1d33
Sebastien Rosset (serosset) <serosset@cisco.com>
2017-09-19 09:22:06 -0700

    Add print statement for debugging

--- dbtest/dbserver.go
+++ dbtest/dbserver.go
@@ -97,6 +97,7 @@ func (dbs *DBServer) execContainer(port int) *exec.Cmd {
     if err == nil {
       break
     } else {
+      fmt.Printf("Failed to pull Mongo container image. err=%s", err.String())
       time.Sleep(5 * time.Second)
     }
   }
