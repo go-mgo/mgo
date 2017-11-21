c0876df8ed59f8e190cb0e28bf6dbdb24d84d1d2
Lucy Cunningham <lcunningham@lyft.com>
2017-11-21 11:35:59 -0800

    Comment

--- session.go
+++ session.go
@@ -4154,12 +4154,14 @@ type Change struct {
 	Upsert    bool        // Whether to insert in case the document isn't found
 	Remove    bool        // Whether to remove the document found rather than updating
 	ReturnNew bool        // Should the modified document be returned rather than the old one
+	Comment   string      // Comment for findAndModify
 }
 
 type findModifyCmd struct {
 	Collection                  string      "findAndModify"
 	Query, Update, Sort, Fields interface{} ",omitempty"
 	Upsert, Remove, New         bool        ",omitempty"
+	Comment                     string      ",omitempty"
 }
 
 type valueResult struct {
